8ea9d13f2783d9e985b2005f558b3b3325543cc1
Sergey <shchegolikhin.sergey@gmail.com>
2025-05-05 19:06:45 +0300

    Update notebook.sql

--- notebook.sql
+++ notebook.sql
@@ -1,9 +1,25 @@
-CREATE TABLE tasks(
-	id SERIAL PRIMARY KEY,
-	user_id INTEGER REFERENCES Users(id) ON DELETE CASCADE,
-        text VARCHAR(100) NOT NULL,
-        is_completed BOOLEAN
+CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(20) NOT NULL,
+        password VARCHAR(16) NOT NULL
 );
 
-INSERT INTO tasks(user_id, text, is_completed) VALUES(1, ' Task number i', false);
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,          
+    text VARCHAR(255) NOT NULL,    
+    user_id INTEGER NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    completed BOOLEAN
+);
 
+INSERT INTO users (username, password) VALUES('1', '1');
+INSERT INTO tasks (text, completed, user_id) VALUES ('Task 1', false, 0);
+
+SELECT text, completed FROM tasks  WHERE user_id = 1;
+SELECT id FROM users WHERE username ='1' AND password = '1';
+
+UPDATE tasks SET completed = true WHERE text ='text 1' AND user_id = 3;
+UPDATE tasks SET text = 'text 2' WHERE text ='text 1'AND user_id = 3;
+
+DELETE FROM users WHERE username ='1' AND password = '1';
+DELETE FROM tasks WHERE text ='text 1' AND user_id = 1;

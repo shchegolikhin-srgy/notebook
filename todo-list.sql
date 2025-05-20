b89a37514fc0e7207d78cf8d3053bc314c1f566f
Sergey <shchegolikhin.sergey@gmail.com>
2025-05-20 19:41:27 +0300

    update sql

--- todo-list.sql
+++ todo-list.sql
@@ -16,19 +16,15 @@ CREATE TABLE tasks (
 
 INSERT INTO users (username, hashed_password) VALUES('1', '1');
 INSERT INTO users (username, hashed_password, role) VALUES('1', '1', 'admin');
-INSERT INTO tasks (text, completed, user_id) VALUES ('Task 1', false, 0);
+INSERT INTO tasks (text, completed, user_id) SELECT $1, $2, users.id FROM users WHERE users.username = $3;
 
 INSERT INTO tasks (text, completed, user_id) SELECT $1, $2, users.id FROM users WHERE users.username = $2;
 
-SELECT tasks.text, tasks.completed
-FROM tasks
-JOIN users ON tasks.user_id = users.id
-WHERE users.username =$1;
-
+SELECT tasks.text, tasks.completed FROM tasks JOIN users ON tasks.user_id = users.id WHERE users.username =$1; 
 SELECT id FROM users WHERE username =$1 AND hashed_password = $2;
 
-UPDATE tasks SET completed = true WHERE text ='text 2' AND user_id = 3;
-UPDATE tasks SET text = 'text 2' WHERE text ='text 1'AND user_id = 3;
+UPDATE tasks SET text = $1 FROM users WHERE tasks.text =$2 AND tasks.user_id = users.id AND users.username = $3;
+UPDATE tasks SET completed = $1 FROM users WHERE tasks.text =$2 AND tasks.user_id = users.id AND users.username = $3;
 
 DELETE FROM users WHERE username ='1' AND hashed_password = '1';
-DELETE FROM tasks WHERE text ='text 1' AND user_id = 1;
+DELETE FROM tasks  USING users WHERE tasks.text =$1 AND tasks.user_id = users.id AND users.username = $2;
